8edf3bbb0b70cec3cbbc54a1954f89a5f804db7f
ebtelmarz <elmarzoukiebtehal@gmail.com>
2020-04-11 11:45:26 +0200

    Update settings.py

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'atcs',
         'USER': 'root',
-        'PASSWORD': 'Minato00!',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
